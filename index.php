54e60df52292a0146397bfc3b9bb08c9e04bb009
esmyth01 <evsmyth@yahoo.com>
2015-10-04 16:04:43 -0700

    First file push adding index.php and style.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+
+<head>
+  <meta charset="utf-8">
+
+  <title>CoSSaR</title>
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+</body>
+</html>
