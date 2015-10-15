c1f5d043ab28f6e376cca5fd56b14bf98d14209f
esmyth01 <evsmyth@yahoo.com>
2015-10-15 16:56:43 -0700

    Setting up the info architecture on Cossar Site
    Right now we have a place holder theme, but we’ll change over to our
    custom theme once it’s more complete

--- page.php
+++ page.php
@@ -1,1 +1,22 @@
-//page.php
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * page.php
+ */
+
+get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'content', 'page' ); ?>
+
+	<?php endwhile; ?>
+  
+  <h2>This is a standard page</h2>
+
+</main><!-- #main -->
+
+<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,1 +1,28 @@
-//single.php
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * single.php
+ */
+
+get_header(); ?>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content', 'single' ); ?>
+
+	        <?php
+
+	        if ( comments_open() || '0' != get_comments_number() )
+	            comments_template();
+	        ?>
+
+		<?php endwhile; ?>
+
+    <h2>This is a single post page</h2>
+
+	</main><!-- #main -->
+
+<?php get_footer(); ?>
