ae17357cb7ff2e8c3237f4bf695cdf7265973131
kimsstewart <stewart_ks@hotmail.com>
2015-10-30 17:29:12 -0700

    modified footer

--- footer.php
+++ footer.php
@@ -1,6 +1,28 @@
 </div><!--end main-content-->
 
 <footer>
+     
+
+<!-- add navigation to page -->
+
+<nav>
+
+<ul>
+<h5>SITEMAP</h5>
+<li>HOME</li>
+    <li> <a href=" http://52.88.123.0/index.php/about/">About</a></li>
+    <li><a href=" http://52.88.123.0/index.php/services/">Services</a></li>
+    <li><a href=" http://52.88.123.0/index.php/projects/">Projects</a></li>
+    <li><a href=" http://52.88.123.0/index.php/publications/">Publications</a></li>
+    <li><a href=" http://52.88.123.0/index.php/blog/">Blog</a></li>
+    <li><a href=" http://52.88.123.0/index.php/contact/">Contact
+<ul>
+<h5>LEGAL</h5> 
+    <li>Terms and Conditions</li>
+    <li>Privavcy Policy</li>
+</ul>    
+
+</nav>
 
     <p> COSSAR &copy; 2015</p>
     
