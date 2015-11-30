d780d1151a3bb220ac13b63be8cb29754152f869
esmyth01 <evsmyth@yahoo.com>
2015-11-29 19:04:49 -0800

    Added a loop to display child page title and excerpt on mobile

--- page.php
+++ page.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * page.php
- */
+* The template for displaying all pages.
+*
+* page.php
+*/
 
 get_header(); ?>
 
@@ -13,36 +13,41 @@ get_header(); ?>
   <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
   <img src="<?php bloginfo('template_directory'); ?>/images/placeholder-page.png" alt="pageholder" class="placeholder-page"/>
-<?php if (have_posts()) : while(have_posts()) : the_post(); //start loop ?>
+  <?php if (have_posts()) : while(have_posts()) : the_post(); //start loop ?>
 
 
-<?php the_content(); ?>
+    <?php the_content(); ?>
 
 
-<?php endwhile; endif;  //end loop?>
-<div id="mobile-nav-div">
-
-<ul class="sub-navigation-mobile"><?php
-
-if ($post->post_parent) {
-  wp_list_pages(array('child_of' => $post->post_parent, 'title_li' => __('')));
-
-} else {
-
-  wp_list_pages(array('child_of' => $post->ID, 'title_li' => __('')));
-
-}
-?></ul>
-</div>
+  <?php endwhile; endif;  //end loop?>
 
 
-</section>
+  <div id="mobile-nav-div">
 
-<aside>
-<?php get_sidebar();?>
-</aside>
-  <h2>This is a standard page</h2>
+    <ul class="sub-navigation-mobile">
+
+      <?php $children = get_pages(array('child_of' => $post->ID));
+
+      foreach ($children as $child) { ?>
+
+        <li><a href="<?php echo get_permalink($child->ID); ?>"><?php echo $child->post_title; ?></a></li>
+
+          <p><?php echo $child->post_excerpt; ?></p>
+
+          <?php } ?>
+
+        </ul>
+
+      </div>
+
+
+    </section>
+
+    <aside>
+      <?php get_sidebar();?>
+    </aside>
+    <h2>This is a standard page</h2>
 
 
 
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
