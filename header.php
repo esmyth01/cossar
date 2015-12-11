d03c8029fda5bb9709809d42e7f03a0f929e1a7b
Brian Baker <brianbaker82@gmail.com>
2015-12-11 10:14:17 -0800

    Added hamburger for mobile nav
    new hamburger graphic for mobile nav

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
 
 <div id="nav-box">
 <div class="jquery-nav">
-				<h4 class="jquery-title"><a href="#">Menu<div class="menu-icon"><img src="<?php bloginfo('template_url'); ?>/images/mobile-nav-cat.png"></div></a></h4>
+				<h4 class="jquery-title"><a href="#">Menu<div class="menu-icon"><img src="<?php bloginfo('template_url'); ?>/images/menu-icon-large.png"></div></a></h4>
 				<?php wp_nav_menu(); ?>
 			</div>
 
