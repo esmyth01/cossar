43c28dc55d5210768d72dfaa26e30de4cd0156f8
esmyth01 <evsmyth@yahoo.com>
2015-12-14 23:13:11 -0800

    changed character length to excerpt function to 70 and gave margin top to sidebar

--- functions.php
+++ functions.php
@@ -92,7 +92,7 @@ $excerpt = get_the_excerpt();
 $excerpt = preg_replace(" (\[.*?\])",'',$excerpt);
 $excerpt = strip_shortcodes($excerpt);
 $excerpt = strip_tags($excerpt);
-$excerpt = substr($excerpt, 0, 60);
+$excerpt = substr($excerpt, 0, 70);
 $excerpt = substr($excerpt, 0, strripos($excerpt, " "));
 $excerpt = trim(preg_replace( '/\s+/', ' ', $excerpt));
 $excerpt = $excerpt.'... <a href="'.$permalink.'">more</a>';
